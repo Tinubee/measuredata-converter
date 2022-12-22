a92ec2610c72e448b45cb1bd34bd7915be9fc9b5
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-12-22 16:39:51 +0900

    Update electron.js

--- public/electron.js
+++ public/electron.js
@@ -1,22 +1,19 @@
-const { app, BrowserWindow, ipcMain, dialog } = require("electron");
-const isDev = require("electron-is-dev");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const { autoUpdater } = require("electron-updater");
-const ProgressBar = require("electron-progressbar");
+const isDev = require("electron-is-dev");
 
-let mainWindow = null;
-let progressBar = null;
+let mainWindow;
 
-const createWindow = () => {
+function createWindow() {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 640,
+    width: 1200,
+    height: 900,
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false,
+      enableRemoteModule: true,
       devTools: isDev,
-      preload: path.join(__dirname, "preload.js"),
     },
+    icon: __dirname + "/logo1024.png",
   });
 
   mainWindow.loadURL(
@@ -29,121 +26,21 @@ const createWindow = () => {
     mainWindow.webContents.openDevTools({ mode: "detach" });
   }
 
-  // mainWindow.setResizable(false);
   mainWindow.setResizable(true);
   mainWindow.on("closed", () => (mainWindow = null));
   mainWindow.focus();
-};
+}
 
-ipcMain.on("app_version", (event) => {
-  event.reply("app_version", { version: app.getVersion() });
-});
+app.on("ready", createWindow);
 
-ipcMain.on("files", async (event) => {
-  const result = await dialog
-    .showOpenDialog(null, {
-      filters: [
-        {
-          name: "Images",
-          extensions: ["jpg", "png"],
-        },
-      ],
-      properties: ["openFile", "multiSelections"],
-    })
-    .then((result) => {
-      const { canceled, filePaths } = result;
-
-      if (canceled) return [];
-      return filePaths;
-    })
-    .catch((err) => {
-      console.log(err);
-      return [];
-    });
-
-  event.reply("files", { files: result });
-});
-
-// 자동으로 업데이트가 되는 것 방지
-autoUpdater.autoDownload = false;
-
-autoUpdater.on("checking-for-update", () => {
-  console.log("업데이트 확인 중");
-});
-
-autoUpdater.on("update-available", () => {
-  console.log("업데이트 버전 확인");
-
-  dialog
-    .showMessageBox({
-      type: "info",
-      title: "Update",
-      message:
-        "새로운 버전이 확인되었습니다. 설치 파일을 다운로드 하시겠습니까?",
-      buttons: ["지금 설치", "나중에 설치"],
-    })
-    .then((result) => {
-      const { response } = result;
-
-      if (response === 0) autoUpdater.downloadUpdate();
-    });
-});
-
-autoUpdater.on("update-not-available", () => {
-  console.log("업데이트 불가");
-});
-
-autoUpdater.once("download-progress", () => {
-  console.log("설치 중");
-
-  progressBar = new ProgressBar({
-    text: "Download 합니다.",
-  });
-
-  progressBar
-    .on("completed", () => {
-      console.log("설치 완료");
-    })
-    .on("aborted", () => {
-      console.log("aborted");
-    });
-});
-
-autoUpdater.on("update-downloaded", () => {
-  console.log("업데이트 완료");
-
-  progressBar.setCompleted();
-
-  dialog
-    .showMessageBox({
-      type: "info",
-      title: "Update",
-      message: "새로운 버전이 다운로드 되었습니다. 다시 시작하시겠습니까?",
-      buttons: ["예", "아니오"],
-    })
-    .then((result) => {
-      const { response } = result;
-
-      if (response === 0) autoUpdater.quitAndInstall(false, true);
-    });
-});
-
-// electron이 초기화 끝났을 때
-app.on("ready", () => {
-  createWindow();
-  autoUpdater.checkForUpdates();
-});
-
-// 모든 window가 종료되었을 때
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
   }
 });
 
-// app이 활성화 되었을 때
 app.on("activate", () => {
-  if (!mainWindow) {
+  if (mainWindow === null) {
     createWindow();
   }
 });
