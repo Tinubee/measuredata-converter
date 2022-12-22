4e5eef1b4b8f8a46d888b6576b3b81f44486d1eb
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-12-22 12:28:08 +0900

    🛠Fix : Updater Fix

--- public/electron.js
+++ public/electron.js
@@ -1,10 +1,15 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, dialog } = require("electron");
 const path = require("path");
 const isDev = require("electron-is-dev");
 const { autoUpdater } = require("electron-updater");
 const log = require("electron-log");
 
 let mainWindow;
+
+function sendStatusToWindow(text) {
+  log.info(text);
+  mainWindow.webContents.send("message", text);
+}
 
 function createWindow() {
   mainWindow = new BrowserWindow({
@@ -36,16 +41,16 @@ function createWindow() {
 /* Updater ======================================================*/
 
 autoUpdater.on("checking-for-update", () => {
-  log.info("업데이트 확인 중...");
+  sendStatusToWindow("업데이트 확인 중...");
 });
 autoUpdater.on("update-available", (info) => {
-  log.info("업데이트가 가능합니다.");
+  sendStatusToWindow("업데이트가 가능합니다.");
 });
 autoUpdater.on("update-not-available", (info) => {
-  log.info("현재 최신버전입니다.");
+  sendStatusToWindow("현재 최신버전입니다.");
 });
 autoUpdater.on("error", (err) => {
-  log.info("에러가 발생하였습니다. 에러내용 : " + err);
+  sendStatusToWindow("에러가 발생하였습니다. 에러내용 : " + err);
 });
 autoUpdater.on("download-progress", (progressObj) => {
   let log_message = "다운로드 속도: " + progressObj.bytesPerSecond;
@@ -57,23 +62,33 @@ autoUpdater.on("download-progress", (progressObj) => {
     "/" +
     progressObj.total +
     ")";
-  log.info(log_message);
+  sendStatusToWindow(log_message);
 });
 autoUpdater.on("update-downloaded", (info) => {
-  log.info("업데이트가 완료되었습니다.");
+  sendStatusToWindow("업데이트가 완료되었습니다.");
+
+  const option = {
+    type: "question",
+    buttons: ["업데이트", "취소"],
+    defaultId: 0,
+    title: "Updater",
+    message: "업데이트가 있습니다. 프로그램을 업데이트 하시겠습니까?",
+  };
+
+  let btnIndex = dialog.showMessageBoxSync(mainWindow, option);
+
+  if (btnIndex === 0) {
+    autoUpdater.quitAndInstall();
+  }
 });
 
-app.on("ready", createWindow);
+app.on("ready", async () => {
+  createWindow();
+  autoUpdater.checkForUpdates();
+});
 
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") {
     app.quit();
-  }
-});
-
-app.on("activate", () => {
-  if (mainWindow === null) {
-    createWindow();
-    autoUpdater.checkForUpdates();
   }
 });
